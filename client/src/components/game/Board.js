8b2ec6d204be7a99af59d8c2f013c2e7737360f7
LilShah <shahidhussain2k13@gmail.com>
2019-05-03 03:22:11 +0500

    .

--- client/src/components/game/Board.js
+++ client/src/components/game/Board.js
@@ -7,7 +7,7 @@ class Board extends Component {
     super(props);
     this.onClick = this.onClick.bind(this);
     this.state = {
-      started: 0,
+      started: false,
       arr: null,
       card: null
     };
@@ -25,14 +25,16 @@ class Board extends Component {
   boardSet = () => {};
   deal = () => {
     this.props.moves.deal();
-    this.setState({ started: this.state.started + 1 });
+    this.setState({ started: true });
     let cards = [];
-    for (var i = 0; i < 13; ++i) {
-      var imgsrc = "/cards/default/" + this.props.G.deck[i][0] + ".svg";
-      let newElement = <img src={imgsrc} id={this.props.G.deck[i][0]} />;
-      cards.push(newElement);
+    if (this.state.started) {
+      for (var i = 0; i < 13; ++i) {
+        var imgsrc = "/cards/default/" + this.props.G.deck[i][0] + ".svg";
+        let newElement = <img src={imgsrc} />;
+        cards.push(newElement);
+      }
+      //game = <div className="cards">a</div>;
     }
-    //game = <div className="cards">a</div>;
     return <tbody>{cards}</tbody>;
   };
   playCard = () => {
@@ -52,7 +54,7 @@ class Board extends Component {
             width: "150px",
             borderRadius: "3px",
             letterSpacing: "1.5px",
-            marginTop: "1rem"
+            margin: "1rem"
           }}
           onClick={this.onClick}
           className="btn btn-large waves-effect waves-light hoverable blue accent-3"
@@ -62,31 +64,29 @@ class Board extends Component {
         {this.boardSet}
         {this.state.arr}
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
         <div className=".column1">
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
-        <img src="/cards/default/RED_BACK(1).svg" ></img>
+          <img src="/cards/default/RED_BACK(1).svg" />
+          <img src="/cards/default/RED_BACK(1).svg" />
         </div>
-
-        
       </div>
     );
   }
