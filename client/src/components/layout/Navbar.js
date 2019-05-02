1ca760dc9c36dfbd7925f96efe15c72ee579b966
Abdul Haseeb <34100036+tigerdrifter@users.noreply.github.com>
2019-05-02 13:46:07 -0700

    colors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
+          <div className="nav-wrapper blue-grey">
             <Link
               to="/"
               style={{
